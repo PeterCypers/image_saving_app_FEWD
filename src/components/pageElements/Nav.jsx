284937238127bb4d8b892cfb69794921817394b9
Peter Cypers <peter.cypers@student.hogent.be>
2023-10-08 16:12:13 +0200

    basic layout created

---
+++ src/components/pageElements/Nav.jsx
@@ -0,0 +1,17 @@
+export default function Nav() {
+    return (
+        <nav id="nav-menu">
+            <ul id="nav-list">
+                <li>
+                    <button>Foto Toevoegen</button>
+                </li>
+                <li>
+                    Mijn Fotos
+                </li>
+                <li>
+                    Mijn Albums
+                </li>
+            </ul>
+        </nav>
+    )
+}
